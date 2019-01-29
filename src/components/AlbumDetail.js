659188c96e56073d7f234c4e7d2a5b1ff96366cf
Nebojsa Konstantinovic <nkonstantinovic@spartagency.com>
2019-01-29 17:16:13 +0100

    part 2

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = props => {
-  const { album } = props;
-  console.log(album);
-
+const AlbumDetail = ({
+  album: { thumbnail_image, title, artist, image, url },
+}) => {
   return (
     <Card>
       <CardSection>
-        {console.log('dasd', album)}
+        <View style={styles.thumbnailContainerStyle}>
+          <Image
+            style={styles.thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text style={styles.headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
 
-        <Text>{album.title}</Text>
+      <CardSection>
+        <Image style={styles.imageStyle} source={{ uri: image }} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>Buy Now</Button>
       </CardSection>
     </Card>
-    // <View>
-    //   <Text>{album.title}</Text>
-    // </View>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
 };
 
 export default AlbumDetail;
